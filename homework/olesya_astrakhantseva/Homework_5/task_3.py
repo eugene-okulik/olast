1ce2089133373339e1f8edd131894d7a730a57fe
olesya <oleastr977@gmail.com>
2024-12-21 21:45:52 +0300

    исправлены замечания по линту

--- homework/olesya_astrakhantseva/Homework_5/task_3.py
+++ homework/olesya_astrakhantseva/Homework_5/task_3.py
@@ -1,13 +1,7 @@
-# Даны такие списки:
-# students = ['Ivanov', 'Petrov', 'Sidorov']
-# subjects = ['math', 'biology', 'geography']
-# Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
-# Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
-
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-students = ', '.join(students) #с помощью ', ' и .join превращаем коллекцию обратно в человекочитаемый вид :)
+students = ', '.join(students)
 subjects = ', '.join(subjects)
 
 print(f'Students {students} study these subjects: {subjects}')
