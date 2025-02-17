6d87ce86502a90de2c8835dd5a2b15ab9711a9a6
olesya <oleastr977@gmail.com>
2025-02-18 00:05:32 +0300

    добавлены пустые строки перед функцией и после

--- homework/olesya_astrakhantseva/Homework_7/task_3.py
+++ homework/olesya_astrakhantseva/Homework_7/task_3.py
@@ -4,9 +4,12 @@ stroki = [
     'result_3 = результат работы программы: 9'
 ]
 
+
 def result(stroki):
     for stroka in stroki:
         number = int(stroka.split()[-1])
         print(number + 10)
 
+
 result(stroki)
+
