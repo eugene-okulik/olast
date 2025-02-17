b8790647f9f492eb42d12e9d7a2faa0a59b2765f
olesya <oleastr977@gmail.com>
2025-02-17 23:37:49 +0300

    Homework_7 done

---
+++ homework/olesya_astrakhantseva/Homework_7/task_3.py
@@ -0,0 +1,12 @@
+stroki = [
+    'result_1 = результат операции: 42',
+    'result_2 = результат операции: 514',
+    'result_3 = результат работы программы: 9'
+]
+
+def result(stroki):
+    for stroka in stroki:
+        number = int(stroka.split()[-1])
+        print(number + 10)
+
+result(stroki)
