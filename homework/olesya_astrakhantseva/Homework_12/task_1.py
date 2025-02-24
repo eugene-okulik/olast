979d86611a0f6ce8ebd8e15b424afa0d243b6dad
olesya <oleastr977@gmail.com>
2025-02-25 00:28:44 +0300

    исправления по pep8

--- homework/olesya_astrakhantseva/Homework_12/task_1.py
+++ homework/olesya_astrakhantseva/Homework_12/task_1.py
@@ -7,22 +7,26 @@ class Flower:
         self.need_water = need_water
         self.color = color
 
+
 class Rose(Flower):
-    def __init__(self, name, height, lifetime, price, need_water, color, petal_count, rose_type):
+    def __init__(self, name, height, lifetime, price, need_water,
+                 color, petal_count, rose_type):
         super().__init__(name, height, lifetime, price, need_water, color)
         self.petal_count = petal_count
         self.rose_type = rose_type
 
 
 class Tulip(Flower):
-    def __init__(self, name, height, lifetime, price, need_water, color, flower_shape, cup_color):
+    def __init__(self, name, height, lifetime, price, need_water,
+                 color, flower_shape, cup_color):
         super().__init__(name, height, lifetime, price, need_water, color)
         self.flower_shape = flower_shape
         self.cup_color = cup_color
 
 
 class Sunflower(Flower):
-    def __init__(self, name, height, lifetime, price, need_water, color, seed_size, sun_tracking_angle):
+    def __init__(self, name, height, lifetime, price, need_water,
+                 color, seed_size, sun_tracking_angle):
         super().__init__(name, height, lifetime, price, need_water, color)
         self.seed_size = seed_size
         self.sun_tracking_angle = sun_tracking_angle
@@ -32,38 +36,31 @@ class Buket:
     def __init__(self):
         self.flowers = []
 
-
     def add_flower(self, flower):
         self.flowers.append(flower)
 
-
     def calc_price(self):
         return sum(flower.price for flower in self.flowers)
-
 
     def average_lifetime(self):
         total_lifetime = sum(flower.lifetime for flower in self.flowers)
         return total_lifetime / len(self.flowers)
 
-
     def sort_by_lifetime(self):
         self.flowers.sort(key=lambda flower: flower.lifetime)
-
 
     def sort_by_color(self):
         self.flowers.sort(key=lambda flower: flower.color)
 
-
     def sort_by_height(self):
         self.flowers.sort(key=lambda flower: flower.height)
-
 
     def sort_by_price(self):
         self.flowers.sort(key=lambda flower: flower.price)
 
-
     def find_flowers_by_lifetime(self, min_lifetime, max_lifetime):
-        return [flower for flower in self.flowers if min_lifetime <= flower.lifetime <= max_lifetime]
+        return [flower for flower in self.flowers
+                if min_lifetime <= flower.lifetime <= max_lifetime]
 
 
 rose = Rose(
@@ -90,10 +87,9 @@ print(f"Среднее время увядания букета: {round(bouquet.
 bouquet.sort_by_lifetime()
 print("Цветы после сортировки по времени жизни:")
 for flower in bouquet.flowers:
-        print(f"- {flower.name}, время жизни: {flower.lifetime} минут")
-
+    print(f"- {flower.name}, время жизни: {flower.lifetime} минут")
 
 found_flowers = bouquet.find_flowers_by_lifetime(min_lifetime=6, max_lifetime=12)
 print("Найденные цветы с временем жизни от 6 до 12 дней:")
 for flower in found_flowers:
-        print(f"- {flower.name}")
+    print(f"- {flower.name}")
