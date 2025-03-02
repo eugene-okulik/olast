07392865a86b29754cfb6f8d740595242b8c4574
Olesya <oleastr977@gmail.com>
2025-03-02 12:39:47 +0300

    исправление запроса на создание студента без группы

--- homework/olesya_astrakhantseva/Homework_14/task_1.sql
+++ homework/olesya_astrakhantseva/Homework_14/task_1.sql
@@ -1,24 +1,26 @@
-INSERT INTO `students` (name, second_name, group_id) VALUES ('Olesya', 'Test', 3024)
+INSERT INTO `students` (name, second_name) VALUES ('Olesya', 'Testovna')
 
-INSERT INTO books (title, taken_by_student_id) VALUES ('QA Python', 4680), ('QA QA', 4680), ('SQL QA QA', 4680)
+INSERT books (title, taken_by_student_id) VALUES ('QA Manual', 4704), ('QA-QA', 4704), ('SQL Learn', 4704)
 
-INSERT INTO `groups` (title, start_date, end_date) VALUES ('test sql', 'march 2024', 'june 2024')
+INSERT INTO `groups` (title, start_date, end_date) VALUES ('test sql 2', 'march 2024', 'july 2024')
 
-INSERT INTO subjets (title) VALUES ('Math QA'), ('Chemistry'), ('Physics')
+UPDATE `students` SET group_id = 3048 WHERE id = 4704;
+
+INSERT INTO subjets (title) VALUES ('Math QA Auto'), ('Chemistry Auto'), ('Physics Auto')
 
 INSERT INTO lessons (title, subject_id) VALUES
-('math lesson one', 4872), ('math lesson two', 4872),
-('chemistry lesson one', 4873), ('chemistry lesson two', 4873),
-('physics lesson one', 4874), ('physics lesson two', 4874)
+('math QA lesson one', 4917), ('math QA lesson two', 4917),
+('chemistry QA lesson one', 4918), ('chemistry QA lesson two', 4918),
+('physics QA lesson one', 4919), ('physics QA lesson two', 4919)
 
 INSERT INTO marks (value, lesson_id, student_id) VALUES
-(4, 8866, 4680), (3, 8867, 4680),
-(5, 8868, 4680), (5, 8869, 4680),
-(3, 8870, 4680), (4, 8871, 4680)
+(4, 8918, 4704), (3, 8919, 4704),
+(5, 8920, 4704), (5, 8921, 4704),
+(3, 8922, 4704), (4, 8923, 4704)
 
-SELECT * FROM marks where student_id = 4680
+SELECT * FROM marks where student_id = 4704
 
-SELECT * FROM books where taken_by_student_id = 4680
+SELECT * FROM books where taken_by_student_id = 4704
 
 SELECT
     s.id AS student_id,
@@ -35,6 +37,6 @@ LEFT JOIN books b ON b.taken_by_student_id = s.id
 LEFT JOIN marks m ON m.student_id = s.id
 LEFT JOIN lessons l ON m.lesson_id = l.id
 LEFT JOIN subjets s2 ON l.subject_id = s2.id
-WHERE s.id = 4680
+WHERE s.id = 4704
 GROUP BY s.id, s.name, s.second_name, g.title, l.title, s2.title, m.value
 ORDER BY l.title;
